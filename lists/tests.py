0afc24a3d2e10e74751afc3efecaaf66fe1bb4e7
kamihati <dream928@gmail.com>
2016-01-02 18:27:03 +0800

    add foreign key to item model

--- lists/tests.py
+++ lists/tests.py
@@ -5,21 +5,28 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
-from lists.models import Item
-
+from lists.models import Item, List
 from lists.views import home_page
 
 
-class ItemModelTest(TestCase):
+class ListAndItemTest(TestCase):
 
 	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+
 		first_item = Item()
 		first_item.text = 'The first (ever) list item'
+		first_item.list = list_
 		first_item.save()
 
 		second_item = Item()
 		second_item.text = 'Item the second'
+		second_item.list = list_
 		second_item.save()
+
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_)
 
 		saved_items = Item.objects.all()
 		self.assertEqual(saved_items.count(), 2)
@@ -27,7 +34,9 @@ class ItemModelTest(TestCase):
 		first_save_item = saved_items[0]
 		second_save_item = saved_items[1]
 		self.assertEqual(first_save_item.text, 'The first (ever) list item')
+		self.assertEqual(first_save_item.list, list_)
 		self.assertEqual(second_save_item.text, 'Item the second')
+		self.assertEqual(second_save_item.list, list_)
 
 
 class HomePageTest(TestCase):
@@ -50,8 +59,9 @@ class ListViewTest(TestCase):
 		self.assertTemplateUsed(response, 'list.html')
 
 	def test_display_all_items(self):
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
+		list_ = List.objects.create()
+		Item.objects.create(text='itemey 1', list=list_)
+		Item.objects.create(text='itemey 2', list=list_)
 
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -68,5 +78,5 @@ class NewListTest(TestCase):
 		self.assertEqual(new_item.text, 'A new list item')
 
 	def test_redirects_after_POST(self):
-		response = self.client.post('/lists/new', dict(item_text='A new list item'))
+		response = self.client.post('/lists/new', data=dict(item_text='A new list item'))
 		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
